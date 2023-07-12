3b37bef995d31cd7fb98d7dfef807eafe7707f99
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-07-12 09:59:29 +0200

    add new project in config

--- config/projects.php
+++ config/projects.php
@@ -1,10 +1,174 @@
 <?php
 return [
     [
+        'title' => 'London Trip',
+        'author' => 'Domenico Ferrari',
+        'url_github' => 'https://github.com/Domenic0Ferrari/html-london-trip',
+        'description' => 'Esercizio di base per HTML, una sorta di diario di bordo.',
+        'type_id' => '1',
+        'technologies' => [
+            '1',
+        ],
+    ],
+    [
+        'title' => 'Toobolist',
+        'author' => 'Domenico Ferrari',
+        'url_github' => 'https://github.com/Domenic0Ferrari/html-css-toboolist',
+        'description' => 'To-do list di cose da fare, aggiunto stile con CSS base.',
+        'type_id' => '1',
+        'technologies' => [
+            '1',
+            '2',
+        ],
+    ],
+    [
+        'title' => 'Float Cards',
+        'author' => 'Domenico Ferrari',
+        'url_github' => 'https://github.com/Domenic0Ferrari/html-css-float-cards',
+        'description' => 'Esercizio di CSS per imparare ad utilizzare il metodo "Float".',
+        'type_id' => '1',
+        'technologies' => [
+            '1',
+            '2',
+        ],
+    ],
+    [
+        'title' => 'Booleaner',
+        'author' => 'Domenico Ferrari',
+        'url_github' => 'https://github.com/Domenic0Ferrari/html-css-booleaner',
+        'description' => 'Esercizio misto di HTML e CSS che simula un articolo scientifico.',
+        'type_id' => '1',
+        'technologies' => [
+            '1',
+            '2',
+        ],
+    ],
+    [
+        'title' => 'Struttura Discord',
+        'author' => 'Domenico Ferrari',
+        'url_github' => 'https://github.com/Domenic0Ferrari/htmlcss-struttura-discord',
+        'description' => 'Esercizio misto di HTML e CSS di preparazione ad uno più complesso.',
+        'type_id' => '1',
+        'technologies' => [
+            '1',
+            '2',
+        ],
+    ],
+    [
+        'title' => 'Discord',
+        'author' => 'Domenico Ferrari',
+        'url_github' => 'https://github.com/Domenic0Ferrari/htmlcss-discord',
+        'description' => 'Esercizio misto di HTML e CSS dove viene simulato il sito web del famoso "Discord".',
+        'type_id' => '1',
+        'technologies' => [
+            '1',
+            '2',
+        ],
+    ],
+    [
+        'title' => 'Boolando',
+        'author' => 'Domenico Ferrari',
+        'url_github' => 'https://github.com/Domenic0Ferrari/html-css-boolando',
+        'description' => 'Esercizio misto di HTML e CSS dove viene simulato il sito web del famoso "Zalando" con aggiunta anche di effetti di hover.',
+        'type_id' => '1',
+        'technologies' => [
+            '1',
+            '2',
+        ],
+    ],
+    [
+        'title' => 'Zoom',
+        'author' => 'Domenico Ferrari',
+        'url_github' => 'https://github.com/Domenic0Ferrari/html-css-zoom',
+        'description' => 'Esercizio misto di HTML e CSS dove viene simulato il sito web del famoso "Zoom".',
+        'type_id' => '1',
+        'technologies' => [
+            '1',
+            '2',
+        ],
+    ],
+    [
+        'title' => 'Animation Filter',
+        'author' => 'Domenico Ferrari',
+        'url_github' => 'https://github.com/Domenic0Ferrari/html-css-animation-filter',
+        'description' => 'Esercizio misto di HTML e CSS per imparare ad usare le animazioni.',
+        'type_id' => '1',
+        'technologies' => [
+            '1',
+            '2',
+        ],
+    ],
+    [
+        'title' => 'Wannabe',
+        'author' => 'Domenico Ferrari',
+        'url_github' => 'https://github.com/Domenic0Ferrari/html-css-resp-wannabe',
+        'description' => 'Esercizio misto di HTML e CSS per imparare il sistema responsive.',
+        'type_id' => '1',
+        'technologies' => [
+            '1',
+            '2',
+        ],
+    ],
+    [
+        'title' => 'Dropbox',
+        'author' => 'Domenico Ferrari',
+        'url_github' => 'https://github.com/Domenic0Ferrari/htmlcss-dropbox',
+        'description' => 'Esercizio misto di HTML e CSS che simula il famoso sito "Dropbox".',
+        'type_id' => '1',
+        'technologies' => [
+            '1',
+            '2',
+        ],
+    ],
+    [
+        'title' => 'Layout Responsive',
+        'author' => 'Domenico Ferrari',
+        'url_github' => 'https://github.com/Domenic0Ferrari/htmlcss-responsive-layout',
+        'description' => 'Esercizio misto di HTML e CSS per costruire un sito completamente responsive.',
+        'type_id' => '1',
+        'technologies' => [
+            '1',
+            '2',
+        ],
+    ],
+    [
         'title' => 'Spotify Web',
         'author' => 'Domenico Ferrari',
         'url_github' => 'https://github.com/Domenic0Ferrari/html-css-spotifyweb',
         'description' => 'Replica del design di Spotify, completamente responsive.',
+        'type_id' => '1',
+        'technologies' => [
+            '1',
+            '2'
+        ],
+    ],
+    [
+        'title' => '12-bool',
+        'author' => 'Domenico Ferrari',
+        'url_github' => 'https://github.com/Domenic0Ferrari/html-css-12bool',
+        'description' => 'Esercizio per imparare ad usare le "col".',
+        'type_id' => '1',
+        'technologies' => [
+            '1',
+            '2'
+        ],
+    ],
+    [
+        'title' => 'Bootstrap Freelancer',
+        'author' => 'Domenico Ferrari',
+        'url_github' => 'https://github.com/Domenic0Ferrari/html-css-bootstrap-freelancer',
+        'description' => 'Primo esercizio con l\'utilizzo di bootstrap.',
+        'type_id' => '1',
+        'technologies' => [
+            '1',
+            '2'
+        ],
+    ],
+    [
+        'title' => 'Bootstrap Dashboard',
+        'author' => 'Domenico Ferrari',
+        'url_github' => 'https://github.com/Domenic0Ferrari/html-css-bootstrap-dashboard',
+        'description' => 'Secondo esercizio con l\'utilizzo di bootstrap.',
         'type_id' => '1',
         'technologies' => [
             '1',
